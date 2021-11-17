5462574ca9ead8165d107cb377b9f38ebd1e78b8
elad3283@gmail.com <zxcvvcxz!123>
2021-11-17 23:26:10 +0200

    downgraded to reatc-router-dom v4.3

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import configureStore from './configureStore';
+import { history } from './configureStore';
+import { Switch, Route } from 'react-router-dom'
+import App from './components/App';
 
 
 const store = configureStore()
@@ -11,14 +14,11 @@ ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <ConnectedRouter history={history}>
-        
+        <Switch>
+          <Route exact path="/" component={App} />
+        </Switch>
       </ConnectedRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
