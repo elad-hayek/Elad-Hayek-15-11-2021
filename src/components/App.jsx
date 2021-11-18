c69dd912cf14e0c05353f0ce184baf50c0e25628
elad3283@gmail.com <zxcvvcxz!123>
2021-11-18 12:46:17 +0200

    creared home, favorites pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import {Switch, Route} from 'react-router-dom'
+import { routes } from '../constants/navigation'
+import Favorites from './Favorites'
+import Home from './Home'
+import NavBar from './NavBar'
 
 const App = () =>{
 
     return(
-        <>
-        <Switch>
-            <Route exact path="/" component={()=>{return(<div>test</div>)}}/>
-        </Switch>
-        </>
+        <div>
+            <NavBar />
+            <Switch>
+                <Route exact path={routes.HOME} component={Home}/>
+                <Route path={routes.FAVORITES} component={Favorites}/>
+            </Switch>
+        </div>
     )
 
 }
