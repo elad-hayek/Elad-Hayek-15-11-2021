c9376bc7d570a1482127d163a1b4df2af53f70ad
elad3283@gmail.com <zxcvvcxz!123>
2021-11-18 13:25:48 +0200

    added font awesome icons

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,13 +2,18 @@ import React from 'react'
 import {Navbar, Container, Nav} from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import { routes } from '../constants/navigation'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faCloudSun} from '@fortawesome/free-solid-svg-icons'
 
 
 const NavBar = () =>{
     return(
-        <Navbar variant="blue">
+        <Navbar bg="light" variant="light">
             <Container>
-                <Navbar.Brand as={Link} to={routes.HOME}>Weahter App</Navbar.Brand>
+                <Navbar.Brand as={Link} to={routes.HOME}>
+                    <FontAwesomeIcon icon={faCloudSun} size="xl" style={{marginRight: 10}}/>
+                    Weahter App
+                </Navbar.Brand>
                 <Nav>
                     <Nav.Link as={Link} to={routes.HOME} >Home</Nav.Link>
                     <Nav.Link as={Link} to={routes.FAVORITES} >Favorites</Nav.Link>
