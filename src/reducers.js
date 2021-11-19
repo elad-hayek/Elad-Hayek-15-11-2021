5584ea2de518d1fe3edcb6ac8e9f524ece61cf43
elad3283@gmail.com <zxcvvcxz!123>
2021-11-19 15:58:42 +0200

    added favorites reducer to reducer list

--- src/reducers.js
+++ src/reducers.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux'
 import { connectRouter } from 'connected-react-router'
+import favoritesReducer from './reducers/favoritesReducer'
 
 const createRootReducer = (history) =>combineReducers({
-    router: connectRouter(history)
+    router: connectRouter(history),
+    favorites: favoritesReducer
 })
 export default createRootReducer
