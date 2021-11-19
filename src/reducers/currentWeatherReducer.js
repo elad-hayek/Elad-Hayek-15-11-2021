f29223592cde4e7fcd81abde3616a3d1a5487c7d
elad3283@gmail.com <zxcvvcxz!123>
2021-11-19 21:19:13 +0200

    create current weather reducer

---
+++ src/reducers/currentWeatherReducer.js
@@ -0,0 +1,27 @@
+import {
+    FETCH_CURRENT_WEATHER_REQUEST,
+    FETCH_CURRENT_WEATHER_SUCCESS,
+    FETCH_CURRENT_WEATHER_FAILURE
+}
+from '../constants/reducer-actions'
+        
+const initialValues = {
+    loading: false, 
+    data: [],
+    error: ''
+}
+        
+export const currentWeatherReducer = (state = initialValues, {type, payload}) => {
+    switch(type){
+        case FETCH_CURRENT_WEATHER_REQUEST:
+            return {...state, loading: true}
+        case FETCH_CURRENT_WEATHER_SUCCESS:
+            return {...state, loading: false, data: payload}
+        case FETCH_CURRENT_WEATHER_FAILURE:
+            return {...state, loading:false, error: payload}
+        default:
+            return state
+    }
+}
+
+export default currentWeatherReducer
