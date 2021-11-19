38b411488b6b10c9ffd59fa179f60e6fa8fa7e89
elad3283@gmail.com <zxcvvcxz!123>
2021-11-19 21:19:33 +0200

    white space removed

--- src/reducers/homeReducer.js
+++ src/reducers/homeReducer.js
@@ -1,19 +1,19 @@
 import {
-UPADTAE_MAIN_LOCATION_TO_VIEW
+    UPADTAE_MAIN_LOCATION_TO_VIEW
 }
 from '../constants/reducer-actions'
     
-    const initialValues = {
-        locationToView: {}
+const initialValues = {
+    locationToView: {}
+}
+
+export const homeReducer = (state = initialValues, {type, payload}) => {
+    switch(type){
+        case UPADTAE_MAIN_LOCATION_TO_VIEW:
+            return {...state, locationToView: payload}
+        default:
+            return state
     }
-    
-    export const homeReducer = (state = initialValues, {type, payload}) => {
-        switch(type){
-            case UPADTAE_MAIN_LOCATION_TO_VIEW:
-                return {...state, locationToView: payload}
-            default:
-                return state
-        }
-    }
-    
-    export default homeReducer
+}
+
+export default homeReducer
