20df5fd20dcd03ae1fb4c81199bd37e4e6f0054d
elad3283@gmail.com <zxcvvcxz!123>
2021-11-19 16:51:15 +0200

    add home reducer to reducer list

--- src/reducers.js
+++ src/reducers.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux'
 import { connectRouter } from 'connected-react-router'
 import favoritesReducer from './reducers/favoritesReducer'
+import homeReducer from './reducers/homeReducer'
 
 const createRootReducer = (history) =>combineReducers({
     router: connectRouter(history),
+    home: homeReducer,
     favorites: favoritesReducer
 })
 export default createRootReducer
