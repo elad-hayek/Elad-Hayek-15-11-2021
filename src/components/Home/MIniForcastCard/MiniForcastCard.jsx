4cfff40bd973609242bd248acb875d25c6b81281
elad3283@gmail.com <zxcvvcxz!123>
2021-11-19 11:02:50 +0200

    created mini forcast card component

---
+++ src/components/Home/MIniForcastCard/MiniForcastCard.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import fiveDayForcast from '../../apivalues/5dayforcastshort.json'
+import currentLocation from '../../apivalues/currentweathershort.json'
+import './MiniForcastCard.css'
+import InformationCard from './InformarionCard/InformationCrad'
+
+
+const MiniForcastCard = () =>{
+
+    return(
+        <div>
+            mini forcast card!
+        </div>
+    )
+
+}
+export default MiniForcastCard
