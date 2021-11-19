3e8fa7ddd7da3638153c950902fc941edaeb4b3c
elad3283@gmail.com <zxcvvcxz!123>
2021-11-19 23:24:31 +0200

    add current weather on first home render

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,18 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import SearchBar from './SearchBar/SearchBar'
 import fiveDayForcast from '../../apivalues/5dayforcastshort.json'
-import currentLocation from '../../apivalues/currentweathershort.json'
+import currentLocationFromdb from '../../apivalues/currentweathershort.json'
 import './Home.css'
 import InformationCard from './InformarionCard/InformationCrad'
+import { useDispatch } from 'react-redux'
+import { updateMainLocationToView } from '../../actionCreators/homeActions'
+import { useSelector } from 'react-redux'
+import { fetchCurrentWeather } from '../../actionCreators/currentWeatherActions'
 
 
 const Home = () =>{
+    const dispatch = useDispatch()
+    
+    useEffect(()=>{
+        dispatch(updateMainLocationToView({id:"215854",name:"tel-aviv", location: currentLocationFromdb[0]}))
+        // dispatch(fetchCurrentWeather("215854"))
+    },[])
 
     return(
         <div>
             <SearchBar/>
             <div className="home-information-container">
-                <InformationCard currentLocation={currentLocation[0]} fiveDayForcast={fiveDayForcast.DailyForecasts}/>
+                <InformationCard fiveDayForcast={fiveDayForcast.DailyForecasts}/>
             </div>
         </div>
     )
