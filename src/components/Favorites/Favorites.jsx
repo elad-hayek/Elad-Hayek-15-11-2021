5d267c59b1353bed7ee65c7cca27043a55ae6051
elad3283@gmail.com <zxcvvcxz!123>
2021-11-19 19:09:58 +0200

    add favorites style for cards

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import './Favorites.css'
+import LocationCard from './LocationCard/LocationCard'
 
 
 const Favorites = () =>{
+    const favorites = useSelector(state=>state.favorites.favorites)
+    let  f  = [...favorites, ...favorites,...favorites,...favorites,...favorites]
 
     return(
-        <div>favorites</div>
+        <div className="favorites-container">
+            <div className="favorites-cards-container">
+                {
+                    [...f,...f,...f].map(location=>{
+                        return(
+                            <LocationCard location={location} />
+                        )
+                    })
+                }
+            </div>
+        </div>
     )
 
 }
