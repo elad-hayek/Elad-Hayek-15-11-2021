7e9583db5b1909c1ac86d027a7f784bfd9adac35
elad3283@gmail.com <zxcvvcxz!123>
2021-11-19 21:18:58 +0200

    add current weather reducer to reducer list

--- src/reducers.js
+++ src/reducers.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux'
 import { connectRouter } from 'connected-react-router'
 import favoritesReducer from './reducers/favoritesReducer'
 import homeReducer from './reducers/homeReducer'
+import currentWeatherReducer from './reducers/currentWeatherReducer'
 
 const createRootReducer = (history) =>combineReducers({
     router: connectRouter(history),
     home: homeReducer,
-    favorites: favoritesReducer
+    favorites: favoritesReducer,
+    currentWeather: currentWeatherReducer
 })
 export default createRootReducer
