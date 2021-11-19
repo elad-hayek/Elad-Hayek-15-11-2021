845b74761a7f4782910821afee04346d118a6e6e
elad3283@gmail.com <zxcvvcxz!123>
2021-11-19 23:23:20 +0200

    add url for current weather and api key

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,1 +1,3 @@
+export const apiKey = "LGugoLfVGSvCt2rtGDuP7oAm38jhBn7N"
 export const weatherIamgesUrl = "https://developer.accuweather.com/sites/default/files/@@-s.png"
+export const currentWeatherUrl = "http://dataservice.accuweather.com/currentconditions/v1/"
