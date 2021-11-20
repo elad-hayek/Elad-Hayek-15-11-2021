5d13e4159ff1185fe92b9e1819e679c455c5fa99
elad3283@gmail.com <zxcvvcxz!123>
2021-11-20 12:24:30 +0200

    add locations reducer to reducers list

--- src/reducers.js
+++ src/reducers.js
@@ -4,12 +4,14 @@ import favoritesReducer from './reducers/favoritesReducer'
 import homeReducer from './reducers/homeReducer'
 import currentWeatherReducer from './reducers/currentWeatherReducer'
 import forcastReducer from './reducers/forcastReducer'
+import locationsReducer from './reducers/locationsReducer'
 
 const createRootReducer = (history) =>combineReducers({
     router: connectRouter(history),
     home: homeReducer,
     favorites: favoritesReducer,
     currentWeather: currentWeatherReducer,
-    forcast: forcastReducer
+    forcast: forcastReducer,
+    locations: locationsReducer
 })
 export default createRootReducer
