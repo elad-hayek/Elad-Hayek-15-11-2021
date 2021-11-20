90e430cd8acf89195f823569ddef4eb4194fafcd
elad3283@gmail.com <zxcvvcxz!123>
2021-11-20 19:56:23 +0200

    add app to reducers

--- src/reducers.js
+++ src/reducers.js
@@ -5,9 +5,11 @@ import homeReducer from './reducers/homeReducer'
 import currentWeatherReducer from './reducers/currentWeatherReducer'
 import forcastReducer from './reducers/forcastReducer'
 import locationsReducer from './reducers/locationsReducer'
+import appReducer from './reducers/appReducer'
 
 const createRootReducer = (history) =>combineReducers({
     router: connectRouter(history),
+    app: appReducer,
     home: homeReducer,
     favorites: favoritesReducer,
     currentWeather: currentWeatherReducer,
