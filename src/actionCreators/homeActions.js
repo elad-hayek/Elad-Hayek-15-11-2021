1731df2ca5d3adbddfcd5a9c6923041a1b3e6cf5
elad3283@gmail.com <zxcvvcxz!123>
2021-11-20 17:35:23 +0200

    add actions to add romve from error stack

--- src/actionCreators/homeActions.js
+++ src/actionCreators/homeActions.js
@@ -1,8 +1,18 @@
 import {
-UPADTAE_MAIN_LOCATION_TO_VIEW
+UPADTAE_MAIN_LOCATION_TO_VIEW,
+ADD_TO_ERROR_STACK,
+REMOVE_FROM_ERROR_STACK
 }
 from '../constants/reducer-actions'
 
 export const updateMainLocationToView = (location) => {
     return { type: UPADTAE_MAIN_LOCATION_TO_VIEW, payload: location }
+}
+
+export const addToErrorStack = (errorObject) =>{
+    return { type: ADD_TO_ERROR_STACK, payload: errorObject}
+}
+
+export const removeFromErrorStack = (id) =>{
+    return { type: REMOVE_FROM_ERROR_STACK, payload: id }
 }
