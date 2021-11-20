ee1f782c1becbf8d936347bd870596dba91bd84f
elad3283@gmail.com <zxcvvcxz!123>
2021-11-20 09:30:03 +0200

    add forcast reducer to reducers list

--- src/reducers.js
+++ src/reducers.js
@@ -3,11 +3,13 @@ import { connectRouter } from 'connected-react-router'
 import favoritesReducer from './reducers/favoritesReducer'
 import homeReducer from './reducers/homeReducer'
 import currentWeatherReducer from './reducers/currentWeatherReducer'
+import forcastReducer from './reducers/forcastReducer'
 
 const createRootReducer = (history) =>combineReducers({
     router: connectRouter(history),
     home: homeReducer,
     favorites: favoritesReducer,
-    currentWeather: currentWeatherReducer
+    currentWeather: currentWeatherReducer,
+    forcast: forcastReducer
 })
 export default createRootReducer
