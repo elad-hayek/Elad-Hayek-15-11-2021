ab315f4f782826e82e70c5c4cbba8877dd88890e
elad3283@gmail.com <zxcvvcxz!123>
2021-11-20 20:18:19 +0200

    add a func to change body color according to theme

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useSelector } from 'react-redux'
 import {Switch, Route} from 'react-router-dom'
+import { themes } from '../../constants/general'
 import {routes} from '../../constants/navigation'
 import Favorites from '../Favorites/Favorites'
 import Home from '../Home/Home'
@@ -7,6 +9,20 @@ import NavBar from '../NavBar/NavBar'
 
 
 const App = () =>{
+    const theme = useSelector(state=>state.app.theme)
+
+    useEffect(()=>{
+        switch(theme){
+            case themes.dark:
+                document.body.className = "body-dark"
+                break
+            case themes.light:
+                document.body.className = "body-light"
+                break
+            default:
+                return
+        }
+    },[theme])
 
     return(
         <div>
