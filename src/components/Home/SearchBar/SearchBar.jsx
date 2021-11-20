0b4030cbe1b391f19c445c490c5e0630c00d163b
elad3283@gmail.com <zxcvvcxz!123>
2021-11-20 23:37:16 +0200

    add search icon to search bar

--- src/components/Home/SearchBar/SearchBar.jsx
+++ src/components/Home/SearchBar/SearchBar.jsx
@@ -8,6 +8,8 @@ import { fetchLocations } from '../../../actionCreators/locationsActions'
 import { fetchCurrentWeather } from '../../../actionCreators/currentWeatherActions'
 import { fetchForcast } from '../../../actionCreators/forcastActions'
 import { objectValidation } from '../../../actionCreators/generalFunctions'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faSearch} from '@fortawesome/free-solid-svg-icons'
 
 
 const SearchBar = () =>{
@@ -32,27 +34,31 @@ const SearchBar = () =>{
         <div className="main-searchbar-container">
             <Container>
                 { locations.error && <small className="search-error-message" >{locations.error}</small>}
-                {/* <AsyncTypeahead 
-                    id="city-serch-autocomplete"
-                    isLoading={locations.loading}
-                    labelKey={option => `${option.Country.LocalizedName} - ${option.LocalizedName}`}
-                    onSearch={(query) => {
-                        dispatch(fetchLocations(query))
-                    }}
-                    options={locations.data}
-                    onChange={(selected)=>{setSelectedCity(selected)}}
-                    selected={selectedCity}
-                    placeholder="Search for a city"
-                    /> */}
-                <Typeahead
-                    className={theme}
-                    id="city-serch-autocomplete"
-                    labelKey={option => `${option.Country.LocalizedName} - ${option.LocalizedName}`}
-                    onChange={(selected)=>{setSelectedCity(selected)}}
-                    options={autocomplete}
-                    placeholder="Search for a city"
-                    selected={selectedCity}
-                />
+                <div className="container-for-adding-icon">
+                    {/* <AsyncTypeahead 
+                        id="city-serch-autocomplete"
+                        className={theme}
+                        isLoading={locations.loading}
+                        labelKey={option => `${option.Country.LocalizedName} - ${option.LocalizedName}`}
+                        onSearch={(query) => {
+                            dispatch(fetchLocations(query))
+                        }}
+                        options={locations.data}
+                        onChange={(selected)=>{setSelectedCity(selected)}}
+                        selected={selectedCity}
+                        placeholder="Search for a city"
+                        /> */}
+                    <Typeahead
+                        className={theme}
+                        id="city-serch-autocomplete"
+                        labelKey={option => `${option.Country.LocalizedName} - ${option.LocalizedName}`}
+                        onChange={(selected)=>{setSelectedCity(selected)}}
+                        options={autocomplete}
+                        placeholder="Search for a city"
+                        selected={selectedCity}
+                    />
+                <FontAwesomeIcon icon={faSearch} size="lg"  flip="horizontal" style={{position:'absolute', top:'23%', right:'5%'}}/>
+                </div>
             </Container>
         </div>
     )
