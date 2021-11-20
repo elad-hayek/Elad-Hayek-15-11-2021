da9a4454cf39f9bce09d1017de811792d56556b6
elad3283@gmail.com <zxcvvcxz!123>
2021-11-20 09:38:02 +0200

    add forcast url to constants

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,3 +1,4 @@
 export const apiKey = "LGugoLfVGSvCt2rtGDuP7oAm38jhBn7N"
 export const weatherIamgesUrl = "https://developer.accuweather.com/sites/default/files/@@-s.png"
 export const currentWeatherUrl = "http://dataservice.accuweather.com/currentconditions/v1/"
+export const forcastUrl = "http://dataservice.accuweather.com/forecasts/v1/daily/5day/"
