4d5a42bc53568f06984ce9b920ef4e6280cc47c4
elad3283@gmail.com <zxcvvcxz!123>
2021-11-20 17:34:34 +0200

    add action to add and remove from error stack

--- src/reducers/homeReducer.js
+++ src/reducers/homeReducer.js
@@ -1,16 +1,23 @@
 import {
-    UPADTAE_MAIN_LOCATION_TO_VIEW
+    UPADTAE_MAIN_LOCATION_TO_VIEW,
+    ADD_TO_ERROR_STACK,
+    REMOVE_FROM_ERROR_STACK
 }
 from '../constants/reducer-actions'
     
 const initialValues = {
-    locationToView: {}
+    locationToView: {},
+    errorStack: []
 }
 
 export const homeReducer = (state = initialValues, {type, payload}) => {
     switch(type){
         case UPADTAE_MAIN_LOCATION_TO_VIEW:
             return {...state, locationToView: payload}
+        case ADD_TO_ERROR_STACK:
+            return {...state, errorStack: state.errorStack.concat(payload)}
+        case REMOVE_FROM_ERROR_STACK:
+            return {...state, errorStack: state.errorStack.filter(error=>error.id !== payload)}
         default:
             return state
     }
