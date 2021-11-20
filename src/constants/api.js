824bf3861342b24e0ed1f389f6e8894215f12757
elad3283@gmail.com <zxcvvcxz!123>
2021-11-20 15:35:07 +0200

    add default location key and name

--- src/constants/api.js
+++ src/constants/api.js
@@ -3,3 +3,4 @@ export const weatherIamgesUrl = "https://developer.accuweather.com/sites/default
 export const currentWeatherUrl = "http://dataservice.accuweather.com/currentconditions/v1/"
 export const forcastUrl = "http://dataservice.accuweather.com/forecasts/v1/daily/5day/"
 export const autocompleteUrl = "http://dataservice.accuweather.com/locations/v1/cities/autocomplete"
+export const defaultLocation = {key: "215854", name:"Tel Aviv"}
