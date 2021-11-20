50348e3b345b708e54f6ebf934e2bdef5d3d6226
elad3283@gmail.com <zxcvvcxz!123>
2021-11-20 13:09:03 +0200

    add autocoplete to search bar

--- src/components/Home/SearchBar/SearchBar.jsx
+++ src/components/Home/SearchBar/SearchBar.jsx
@@ -1,15 +1,43 @@
-import React from 'react'
-import { Container, InputGroup } from 'react-bootstrap'
+import React, { useEffect, useState } from 'react'
+import { Container } from 'react-bootstrap'
 import Form from 'react-bootstrap/Form'
 import './SearchBar.css'
+import { AsyncTypeahead, Typeahead } from 'react-bootstrap-typeahead'
+import autocomplete from '../../../apivalues/autocomplete.json'
+import { useSelector, useDispatch } from 'react-redux'
+import { fetchLocations } from '../../../actionCreators/locationsActions'
 
 
 const SearchBar = () =>{
+    const locations = useSelector(state=>state.locations)
+    const dispatch = useDispatch()
+    const [selectedCity, setSelectedCity] = useState([])
+
+    useEffect(()=>{console.log(selectedCity)},[selectedCity])
 
     return(
         <div className="main-searchbar-container">
             <Container>
-                    <Form.Control  placeholder="Search for a city" />
+                {/* <AsyncTypeahead 
+                    id="city-serch-autocomplete"
+                    isLoading={locations.loading}
+                    labelKey={option => `${option.Country.LocalizedName} - ${option.LocalizedName}`}
+                    onSearch={(query) => {
+                        dispatch(fetchLocations(query))
+                    }}
+                    options={locations.data}
+                    onChange={(selected)=>{setSelectedCity(selected)}}
+                    selected={selectedCity}
+                /> */}
+                <Typeahead
+                    id="city-serch-autocomplete"
+                    labelKey={option => `${option.Country.LocalizedName} - ${option.LocalizedName}`}
+                    onChange={(selected)=>{setSelectedCity(selected)}}
+                    options={autocomplete}
+                    placeholder="Search for a city"
+                    selected={selectedCity}
+                />
+                {/* <Form.Control  placeholder="Search for a city" ref={searchRef} onChange={handleChange}/> */}
             </Container>
         </div>
     )
